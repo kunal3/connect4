c3fdfa2866958786444090664a9ba5814b9493cc
kunal3 <kunalagrawal05@gmail.com>
2017-05-07 15:26:41 -0400

    Initial commit with some ideas

---
+++ main.rb
@@ -0,0 +1,14 @@
+class Board
+  # Board x by x rows by cols
+  # Piece - knows its own color
+  # Board could be 0, 1, 2 - empty, red, black
+  # Piece could know color and pos
+RED = 1
+BLACK = 2
+
+
+  def end?
+    
+  end
+
+end
